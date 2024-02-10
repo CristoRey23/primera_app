f60896aa27690c7584ea781836386a1d1f166853
PC14 <pc14@iMac-de-PC14.local>
2024-02-10 10:44:18 -0600

    primer comit

---
+++ primera_app/src/MyComponent.js
@@ -0,0 +1,12 @@
+export function MyComponent()
+{
+  return <h1>Este es mi componente</h1>
+}
+export function Presentacion ()
+{
+  return <div><h1>Mi nombre es Cristoffer Diaz</h1>
+  <h3>mi edad es 20 anios</h3>
+  <p>trabajo en la makila</p>
+  </div>
+
+}
